98ff58ce179242116a644045fd289a51b327e7b1
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-07-26 13:44:20 +0100

    Initial Commit

---
+++ atis/atisApp.swift
@@ -0,0 +1,17 @@
+//
+//  atisApp.swift
+//  atis
+//
+//  Created by Bolaji Olajide on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct atisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
